e9d9cc50a0475c2ab127f8b89481481217411b47
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-01-18 17:31:11 +0900

    install 先変更

--- utilities/express-generator/install.sh
+++ utilities/express-generator/install.sh
@@ -3,5 +3,5 @@
 # このファイルのフォルダ名を取得
 cur_dir=$(cd $(dirname $0); pwd)
 
-# run.sh のシンボリックリンクを utilities に express という名前で作成する
-ln -s $cur_dir/run.sh $cur_dir/../../../utilities/express
+# run.sh のシンボリックリンクを ~/bin/express という名前で作成する
+ln -s $cur_dir/run.sh $HOME/bin/express
