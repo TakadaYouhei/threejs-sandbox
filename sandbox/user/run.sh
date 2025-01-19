7074ff7e49c476c64ae2af761d268547553759ae
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-01-19 10:51:45 +0900

    修正

--- sandbox/user/run.sh
+++ sandbox/user/run.sh
@@ -3,6 +3,6 @@
 IMAGE=sandbox_user:latest
 
 #docker run -v .:/tmp/express --rm -it --user=$USER ${IMAGE} $@
-docker run -v .:/tmp/express --rm -it ex${IMAGE} $@
+docker run -v .:/tmp/express --rm -it ${IMAGE} $@
 
 
