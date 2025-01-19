198eaed759d1e9f6ff17f351fe9f533b73d190ae
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-01-19 17:54:47 +0900

    まだ docker run でエラーになる

--- utilities/express-generator/run.sh
+++ utilities/express-generator/run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-docker run -v .:/tmp/express --rm -it --user=$USER express express $@
+USERID=$(id -u)
+GROUPID=$(id -g)
+
+docker run -v .:/tmp/express --rm -it --user=$USER -e USERID=$USERID -e GROUPID=$GROUPID express express $@
 
