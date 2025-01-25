a4a80a2c1dc64f3ab1fba60454b40d429417f37b
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-01-25 16:37:15 +0900

    npm 起動時に PORT マッピングするように
    npm start でデーモン起動したものにホストからアクセスできるように

--- utilities/express-generator/run_npm.sh
+++ utilities/express-generator/run_npm.sh
@@ -2,6 +2,16 @@
 
 USERID=$(id -u)
 GROUPID=$(id -g)
+PORT=3000
 
-docker run -v .:/tmp/express --rm -it -e USERID=$USERID -e GROUPID=$GROUPID express npm $@
+docker run \
+    -v .:/tmp/express \
+    -p $PORT:$PORT \
+    --rm \
+    -it \
+    -e USERID=$USERID \
+    -e GROUPID=$GROUPID \
+    -e PORT=$PORT \
+    express \
+    npm $@
 
