304ed9a1a59d18652640446be5eaba62d4117a52
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-02-09 22:35:53 +0900

    git clone で証明書の確認がきたら自動で yes にするようにしてみた
    セキュリティ的に微妙だが・・・

--- app3/for_cicl/commit_build_results.sh
+++ app3/for_cicl/commit_build_results.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
-git clone git@github.com:TakadaYouhei/threejs-sandbox-output.git output_git
+# The authenticity of host 'github.com (x.x.x.x)' can't be established.
+# auto yes
+echo yes | git clone git@github.com:TakadaYouhei/threejs-sandbox-output.git output_git
 cp -r ~/artifacts/* output_git/
 pushd output_git
 git add *
