c44809f6c8c6c01c157beffc8d36e8a0fcccd778
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-02-09 22:07:37 +0900

    git commit & push する処理記載

--- app3/for_cicl/commit_build_results.sh
+++ app3/for_cicl/commit_build_results.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
 
-pwd
-ls
+git clone git@github.com:TakadaYouhei/threejs-sandbox-output.git output_git
+cp -r ~/artifacts/* output_git/
+pushd output_git
+git add *
+git commit -m "Add build results"
+git push origin main
+popd
+
