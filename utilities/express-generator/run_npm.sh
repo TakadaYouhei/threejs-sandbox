5dd136cb457ec28a49ef401bea70b39c2ac45a1f
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-02-11 10:55:44 +0900

    npm に 5173 port 追加
    vite 用

--- utilities/express-generator/run_npm.sh
+++ utilities/express-generator/run_npm.sh
@@ -3,10 +3,12 @@
 USERID=$(id -u)
 GROUPID=$(id -g)
 PORT=3000
+PORT2=5173
 
 docker run \
     -v .:/tmp/express \
     -p $PORT:$PORT \
+    -p $PORT2:$PORT2 \
     --rm \
     -it \
     -e USERID=$USERID \
