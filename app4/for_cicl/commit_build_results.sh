212d42f51b142cbb1209e8585f44d4b462d6f563
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-02-11 21:10:51 +0900

    git commit するシェルを app3 からコピー

---
+++ app4/for_cicl/commit_build_results.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+ls -al ~/.ssh
+cat ~/.ssh/known_hosts
+
+GIT_SSH_COMMAND="ssh -o BatchMode=yes" git clone git@github.com:TakadaYouhei/threejs-sandbox-output.git output_git
+cp -r ~/artifacts/* output_git/
+pushd output_git
+
+git add *
+git commit -m "Add build results"
+git push origin main
+popd
+
