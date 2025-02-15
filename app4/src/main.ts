9dab1b6106fbc0b7584db030bda9c59c9ad1cd12
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-02-15 09:26:11 +0900

    追加漏れ

--- app4/src/main.ts
+++ app4/src/main.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
-checkWebGL();
+checkWebGL2();
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -18,7 +18,7 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-function checkWebGL() {
+function checkWebGL2() {
 	if (WebGL.isWebGL2Available()) {
 		console.log('WebGL is available');
 	} else {
