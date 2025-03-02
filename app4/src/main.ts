d199a3fc4aec815b13d7c57264eb2847acab8549
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-03-02 12:59:01 +0900

    renderer 復活
    menu の挙動が安定したので戻す

--- app4/src/main.ts
+++ app4/src/main.ts
@@ -11,7 +11,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
-//document.body.appendChild( renderer.domElement );
+document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
