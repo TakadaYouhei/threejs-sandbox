1a137d99cdc8db12b1a3d5e88b1b7e134e5b3cb3
TakadaYouhei <TakadaYouhei@users.noreply.github.com>
2025-03-15 10:47:49 +0900

    iscene インターフェース定義

--- app4/src/sys/iscene.ts
+++ app4/src/sys/iscene.ts
@@ -1,9 +1,45 @@
 /**
  * SceneManager で管理されるシーン情報
+ *
+ * 呼び出され方
+ * init()
+ * loadAsync()
+ * enterScene()
+ *
+ * exitScene()
+ 
  */
 interface IScene{
-  // シーン表示に必要な読み込みを行う
-  on_load(): Promise<void>
+  // 初期化を行う
+  init(): void
+  
+  /**
+   * シーン表示に必要な読み込みを行う非同期メソッド
+   * 読み込み処理が完了するまで待機する。
+   *
+   * @returns Promise<void> - 読み込み処理が正常に完了した時に解決されるPromise
+   */
+  loadAsync(): Promise<void>
+  
+  /**
+   * シーンの表示を開始する非同期メソッド。
+   * シーンの表示開始の演出が完了するまで待機する。
+   * 
+   * @returns Promise<void> - シーン表示が正常に完了したときに解決されるPromise
+   */
+  enterScene(): Promise<void>
+  
+  /**
+   * アニメーション処理
+   * 
+   * @param float dt 前回呼び出してからの経過時間[秒]
+   */
+  animate(dt: float): void
+  
+  /**
+   * シーン表示終了時に呼ばれる。
+   */
+  exitScene(): Promise<void>
 }
 
 export type { IScene }
